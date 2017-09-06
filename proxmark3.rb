66762b47f5beaa155c65832a259de2dba1411839
iceman1001 <iceman@iuse.se>
2017-09-06 21:17:21 +0200

    FIX: some needed files is missing when using homebrew. They don't get copied to the binary folder

--- proxmark3.rb
+++ proxmark3.rb
@@ -6,8 +6,6 @@ class Proxmark3 < Formula
   sha256 "c62f67f5d037af002aeb2987289c8e593b8850ebb10cebc37293f3f3695bd032"
   head "https://github.com/iceman1001/proxmark3.git"
 
-											   
-
   depends_on "automake" => :build
   depends_on "readline"
   depends_on "p7zip" => :build
@@ -25,11 +23,35 @@ class Proxmark3 < Formula
 #    system "make", "-C", "client/hid-flasher/"
     system "make", "clean"	  
     system "make", "all"
+
     bin.mkpath
     bin.install "client/flasher" => "proxmark3-flasher"
 #    bin.install "client/hid-flasher/flasher" => "proxmark3-hid-flasher"
     bin.install "client/proxmark3" => "proxmark3"
     bin.install "client/fpga_compress" => "fpga_compress"
+#	bin.install "tools/mfkey/mfkey32" => "mfkey32"
+#	bin.install "tools/mfkey/mfkey64" => "mfkey64"
+
+	#default keys
+	bin.install "client/default_keys.dic" => "default_keys.dic"
+	bin.install "client/default_pwd.dic" => "default_pwd.dic"
+	
+	# hardnested binary bit_flip files
+	(bin/"hardnested").mkpath
+	(bin/"hardnested").install Dir["client/hardnested/*"]
+
+	# lua libs for proxmark3 scripts
+	(bin/"lualibs").mkpath
+	(bin/"lualibs").install Dir["client/lualibs/*"]
+
+	# lua scripts
+	(bin/"scripts").mkpath
+	(bin/"scripts").install Dir["client/scripts/*"]
+	
+	# trace files for experimentations
+	(bin/"traces").mkpath
+	(bin/"traces").install Dir["traces/*"]
+
     share.mkpath
     (share/"firmware").mkpath
     (share/"firmware").install "armsrc/obj/fullimage.elf" => "fullimage.elf"
